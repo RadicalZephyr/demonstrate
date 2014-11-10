ddf8481e0ec69d77d79017a4263ddee904346ee2
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-10 16:05:37 -0500

    Add an mli file for the pty module

---
+++ pty.mli
@@ -0,0 +1,14 @@
+module Pt_options :
+  sig
+    type pt_options = ReadWrite | NoCTTY
+    val of_int : int -> pt_options
+    val to_int : pt_options -> int
+  end
+
+val pt_opt : Pt_options.pt_options Ctypes.typ
+val file_desc : UnixLabels.file_descr Ctypes.typ
+
+val openpt : Pt_options.pt_options -> UnixLabels.file_descr
+val grantpt : UnixLabels.file_descr -> bytes
+val ptsname : UnixLabels.file_descr -> int
+val unlockpt : UnixLabels.file_descr -> int
