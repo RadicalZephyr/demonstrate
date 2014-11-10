f991619324978df1fa1a9456e6ebd7f579bf23bb
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-10 14:22:43 -0500

    Process arguments with pattern matching

--- demonstrate.ml
+++ demonstrate.ml
@@ -1,8 +1,15 @@
 open Core.Std
 
 
+let print_usage () =
+  print_string "usage: demonstrate <script> <interpreter> [args...]\n"
 
-
+let print_args script interpreter args =
+  printf "Process script: '%s' with interpreter: '%s'\n with args '%s'\n"
+         script interpreter (String.concat args ~sep:", ")
 
 let () =
-  print_string "Hello World!"
+  match (Array.to_list Sys.argv) with
+  | [] | _ :: [] | _ :: _ :: [] -> print_usage ()
+  | _ :: script :: interpreter :: args ->
+       print_args script interpreter args
