67264da5bc0c41582156b962068a56915bf061e2
Geoff Shannon <geoffpshannon@gmail.com>
2014-11-10 15:48:45 -0500

    Create some views to make the ctypes wrappers more expressive

--- pty.ml
+++ pty.ml
@@ -1,14 +1,42 @@
+open Core.Std
 open Ctypes
 open Foreign
 
+module Pt_options = struct
+    type pt_options =
+      | ReadWrite
+      | NoCTTY
+
+    let of_int num =
+      match num with
+      | 0 -> ReadWrite
+      | 1 -> NoCTTY
+      | x -> failwith (sprintf "Error, cannot use %d as an option for openpt" x)
+
+    let to_int opt =
+      match opt with
+      | ReadWrite -> 0
+      | NoCTTY    -> 1
+  end
+
+let pt_opt =
+  view int
+       ~read:Pt_options.of_int
+       ~write:Pt_options.to_int
+
+let file_desc =
+  view int
+       ~read:Unix.File_descr.of_int
+       ~write:Unix.File_descr.to_int
+
 let openpt =
-  foreign "posix_openpt" (int @-> returning int)
+  foreign "posix_openpt" (pt_opt @-> returning file_desc)
 
 let grantpt =
-  foreign "grantpt" (int @-> returning string)
+  foreign "grantpt" (file_desc @-> returning string)
 
 let ptsname =
-  foreign "ptsname" (int @-> returning int)
+  foreign "ptsname" (file_desc @-> returning int)
 
 let unlockpt =
-  foreign "unlockpt" (int @-> returning int)
+  foreign "unlockpt" (file_desc @-> returning int)
